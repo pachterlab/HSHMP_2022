80c57669be5b9d7930f4ba888412fee8869b7d75
Kristjan Eldjarn <kristjan@eldjarn.net>
2022-09-28 17:14:44 -0700

    added comments

--- hg38_dna/tile_genome.py
+++ hg38_dna/tile_genome.py
@@ -5,13 +5,16 @@ import gzip
 
 def write_entry(out, head, body, FA_WIDTH=80):
     out.write(str.encode(f'{head}\n'))
-    out.write(str.encode(('\n'.join(body[i:i+FA_WIDTH] for i in range(0, len(body), FA_WIDTH)))))
+    # Break down the body into chunks of length FA_WIDTH for legibility
+    out.write(str.encode('\n'.join(body[i:i+FA_WIDTH]
+                         for i in range(0, len(body), FA_WIDTH))))
     out.write(str.encode('\n'))
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Create a tiling cover of the\
+                                                  supplied assembly scaffold file.')
     parser.add_argument('--length', type=int, default=500, help='Length of each tile')
     parser.add_argument('--fa', type=str, help='Genome assembly')
     parser.add_argument('--out', type=str, help='Output file')
@@ -29,14 +32,16 @@ if __name__ == '__main__':
 
             l = line.decode('utf-8').rstrip('\n')
             if l[0] == '>':
+                # Start new scaffold
 
-                if gene != '':
+                if gene != '' and len(tile) > 0:
+                    # Write the last tile in last scaffold if this is not the
+                    # first scaffold and there is a tile to be written
                     write_entry(out, f'{gene}.{tile_n}', tile)
 
                 gene = l.split()[0]
                 tile_n = 1
                 tile = ''
-                print('gene', gene)
                 continue
 
             i = 0
@@ -45,7 +50,6 @@ if __name__ == '__main__':
                 i += 1
             if i == len(l) - 1:
                 continue
-
             l = l[i:]
 
             left = args.length - len(tile)
@@ -57,7 +61,7 @@ if __name__ == '__main__':
                 tile = l[endp:]
 
         # Write last entry if length of tile was not an integer multiple of the
-        # total length of the file
-        if len(tile) == args.length:
+        # total length of the last scaffold
+        if len(tile) > 0:
             write_entry(out, f'{gene}.{tile_n}', tile)
     out.close()
