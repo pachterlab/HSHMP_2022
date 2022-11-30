7ccaba30e34ebe17aff6eef3ad383cc23d6d7138
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2022-11-29 20:36:51 -0800

    Update gen_unique_barcodes.sh

--- nascent_vs_mature_examples/gen_unique_barcodes.sh
+++ nascent_vs_mature_examples/gen_unique_barcodes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Generate 10XV3 R1 read sequences with random barcodes and GGGGGGGG as UMI
+# Generate 10XV3 R1 read sequences with random barcodes and GGGGGGGGGGGG as UMI
 # Usage: gen_unique_barcodes.sh <number_of_reads>
+# (If a second argument is supplied, a random 12-bp UMI will also be generated rather than GGGGGGGGGGGG)
 array=("A" "T" "C" "G")
 end=$(($1/4))
 for (( x=0; x<$end; x++ ))
@@ -15,7 +16,12 @@ do
 		str=$str$chh
 		shh=$(($shh-1))
 	done
-	echo "$str"GGGGGGGGGGGG
+        if [ -z "$2" ]
+        then
+                echo "$str"GGGGGGGGGGGG
+        else
+                echo "$str"${str:4:16}
+        fi
         echo "+"
         echo "KKKKKKKKKKKKKKKKKKKKKKKKKKKK"
 done
